5efb409cad2363dc987393540528103bbb23c807
PRIYANKA <priyanka.raut@healthcoco.com>
2026-03-11 15:25:28 +0530

    UI FIX

--- src/pages/ClinicDetails.tsx
+++ src/pages/ClinicDetails.tsx
@@ -1104,10 +1104,11 @@ const ClinicDetails = () => {
         contextData={contextData}
       />
 
-      {/* KPI Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {/* KPI Cards - single row on large screens */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
         {(performanceChartsLoading || monthlySummaryLoading) && (
           <>
+            <Skeleton className="h-32 w-full" />
             <Skeleton className="h-32 w-full" />
             <Skeleton className="h-32 w-full" />
             <Skeleton className="h-32 w-full" />
